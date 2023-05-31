d8a79e3f65076df1634ba2b83400a778f78537c4
Renij Shrestha <renijshrestha8@gmail.com>
2023-05-31 12:55:00 +1000

    touch up

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -1,6 +1,18 @@
 const { Pool } = require('pg')
  
-const pool = new Pool()
+const host = process.env.HOST;
+const user = process.env.USER;
+const password = process.env.PASSWORD;
+const database = process.env.DB;
+const port = process.env.PORT;
+
+const pool = new Pool({
+    user: user,
+    host: host,
+    database: database,
+    password: password,
+    port: port
+})
   
 
 module.exports = pool;
