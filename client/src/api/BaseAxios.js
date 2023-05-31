1a0253a290457690f107d5a1445c720736811db2
Renij Shrestha <renijshrestha8@gmail.com>
2023-05-31 15:26:17 +1000

    env

--- client/src/api/BaseAxios.js
+++ client/src/api/BaseAxios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 export default axios.create({
-    baseURL: SERVER_URL || "http://localhost:4000/api",
+    baseURL: SERVER_URL || "http://localhost:5005/api",
 })
--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -1,17 +1,10 @@
 const { Pool } = require('pg')
  
-const host = process.env.HOST;
-const user = process.env.USER;
-const password = process.env.PASSWORD;
-const database = process.env.DB;
-const port = process.env.DB_PORT;
+
+const dabtabaseURL = process.env.EXTERNAL_URL || process.env.INTERNAL_URL;
 
 const pool = new Pool({
-    user: user,
-    host: host,
-    database: database,
-    password: password,
-    port: port
+    connectionString: dabtabaseURL,
 })
   
 
