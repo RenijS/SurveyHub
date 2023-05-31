fc294240ff1c3a0ca94892afd87c444ecc29eccf
Renij Shrestha <renijshrestha8@gmail.com>
2023-05-31 13:50:32 +1000

    env

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -4,7 +4,7 @@ const host = process.env.HOST;
 const user = process.env.USER;
 const password = process.env.PASSWORD;
 const database = process.env.DB;
-const port = process.env.PORT;
+const port = process.env.DB_PORT;
 
 const pool = new Pool({
     user: user,
