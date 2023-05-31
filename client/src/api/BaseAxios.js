90ab3ca768f195cf79cdaa0451a21da0a68444e1
Renij Shrestha <renijshrestha8@gmail.com>
2023-05-31 14:12:10 +1000

    env

--- client/src/api/BaseAxios.js
+++ client/src/api/BaseAxios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
 export default axios.create({
-    baseURL: process.env.REACT_APP_SERVER_URL || "http://localhost:4000/api",
+    baseURL: SERVER_URL || "http://localhost:4000/api",
 })
